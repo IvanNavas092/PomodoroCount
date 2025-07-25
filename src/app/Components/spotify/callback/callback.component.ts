94867ab2c0233b44f7d2e2b90d8f95ed860e8362
Ivan Navas <navass21112@gmail.com>
2025-07-25 12:37:13 +0200

    add environment an change the callback in production

--- src/app/Components/spotify/callback/callback.component.ts
+++ src/app/Components/spotify/callback/callback.component.ts
@@ -27,7 +27,7 @@ export class CallbackComponent implements OnInit {
       .set('client_id', '8a3f3c699a324421b023cd845ef8aa4a')
       .set('grant_type', 'authorization_code')
       .set('code', code)
-      .set('redirect_uri', 'https://597e9fb4390e.ngrok-free.app/callback')
+      .set('redirect_uri', 'https://pomodorocount.netlify.app/callback')
       .set('code_verifier', codeVerifier);
 
     const headers = new HttpHeaders({
